fddc47d4ec5aa2a36528a4e0756f934f3f20fc3f
Pietro Berkes <pietro.berkes@googlemail.com>
2022-06-04 14:28:12 +0200

    Dosctrings for Datastore; add tests for exceptions advertised in doscstring

--- pond/storage/datastore.py
+++ pond/storage/datastore.py
@@ -3,10 +3,45 @@ from io import BytesIO
 
 
 class Datastore(ABC):
+
     @abstractmethod
-    def read(self, relative_path: str) -> BytesIO:
+    def read(self, path: str) -> BytesIO:
+        """Read a sequence of bytes from the data store.
+
+        Parameters
+        ----------
+        path: str
+            Path relative to the root of the data store.
+
+        Returns
+        -------
+        BytesIO
+            The sequence of bytes read from `path`.
+
+        Raises
+        ------
+        FileNotFoundError
+            If the requested path does not exist.
+        """
         pass
 
     @abstractmethod
-    def write(self, relative_path: str, data: BytesIO) -> None:
+    def write(self, path: str, data: BytesIO) -> None:
+        """Write a sequence of bytes to the data store.
+
+        `path` contains the path relative to the root of the data store, including the name
+        of the file to be created. If a file already exists at `path`, it is overwritten.
+
+        Parameters
+        ----------
+        path: str
+            Path relative to the root of the data store.
+        data: BytesIO
+            Sequence of bytes to write at `path`.
+
+        Raises
+        ------
+        FileNotFoundError
+            If the path where the data is to be written does not exist (excluding file name).
+        """
         pass
--- tests/storage/test_file_datastore.py
+++ tests/storage/test_file_datastore.py
@@ -3,7 +3,20 @@ from pond.storage.file_datastore import FileDatastore
 import pytest
 
 
-def test_file_datastore_read(tmp_path):
+def test_base_path_does_not_exist(tmp_path):
+    # path does not exist
+    with pytest.raises(FileNotFoundError):
+        FileDatastore('does_not_exist')
+
+    # path exists, but it's not a directory
+    filename = 'mydata'
+    filepath = tmp_path / filename
+    filepath.touch()
+    with pytest.raises(NotADirectoryError):
+        FileDatastore(filepath)
+
+
+def test_read(tmp_path):
     content = b'A test! 012'
     filename = 'mydata.bin'
     filepath = tmp_path / filename
@@ -15,28 +28,27 @@ def test_file_datastore_read(tmp_path):
     assert data == content
 
 
-def test_file_datastore_path_does_not_exist(tmp_path):
-    # path does not exist
-    with pytest.raises(RuntimeError):
-        FileDatastore('does_not_exist')
-
-    # path exists, but it's a file
-    filename = 'mydata'
-    filepath = tmp_path / filename
-    filepath.touch()
-
-    with pytest.raises(RuntimeError):
-        FileDatastore(filepath)
+def test_read_file_not_found(tmp_path):
+    ds = FileDatastore(tmp_path)
+    with pytest.raises(FileNotFoundError):
+        ds.read('does_not_exist')
 
 
-def test_file_datastore_write(tmp_path):
+def test_write(tmp_path):
     data = b'A test! 012'
     filename = 'mydata.bin'
+    filepath = tmp_path / filename
 
     ds = FileDatastore(tmp_path)
     ds.write(filename, data)
 
-    filepath = tmp_path / filename
     content = filepath.read_bytes()
-
     assert data == content
+
+
+def test_write_file_path_does_not_exist(tmp_path):
+    data = b'A test! 012'
+
+    ds = FileDatastore(tmp_path)
+    with pytest.raises(FileNotFoundError):
+        ds.write('does_not_exits/blah', data)
