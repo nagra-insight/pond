c1711fe315d346ff3700dc45e36a27b5f08f576d
Pietro Berkes <pietro.berkes@googlemail.com>
2022-07-01 11:22:07 +0200

    Conventions: location of the manifest file

---
+++ pond/conventions.py
@@ -0,0 +1,12 @@
+MANIFEST_FILENAME = 'manifest.yml'
+METADATA_DIRNAME = '_pond'
+
+
+def manifest_location(version_location: str) -> str:
+    """ Manifest location with respect to a version root. """
+    return urijoinpath(version_location, METADATA_DIRNAME, MANIFEST_FILENAME)
+
+
+def urijoinpath(*parts) -> str:
+    """Joins two uri path components, also ensure the right part does not end with a slash"""
+    return '/'.join([part.rstrip('/') for part in parts])
