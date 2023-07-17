a3fce54eb3dd74efddf595b92e3ad09a44f9ae34
Pietro Berkes <pietro.berkes@gmail.com>
2023-07-17 02:45:14 -0600

    Working on Activity

--- pond/activity.py
+++ pond/activity.py
@@ -1,30 +1,53 @@
-from typing import Generic, Optional, Union, Sequence, Dict
+from typing import Dict, Optional, Sequence, Type, Union
 
-from pond.version_name import VersionName
+from pond.artifact import Artifact
+from pond.conventions import DataType, SaveMode
+from pond.storage.datastore import Datastore
+from pond.version import Version
+from pond.version_name import SimpleVersionName, VersionName
+from pond.versioned_artifact import VersionedArtifact
 
-from .adapter import Adapter, DataType, SaveMode
-from .storage import Datastore
 
-
-class Activity(Generic[DataType]):
-    def __init__(self, source: str, datastore: Datastore, author: str):
-        pass
+class Activity:
+    def __init__(self,
+                 source: str,
+                 location: str,
+                 datastore: Datastore,
+                 author: str,
+                 version_name_class=SimpleVersionName):
+        self.source = source
+        self.location = location
+        self.datastore = datastore
+        self.author = author
+        self.version_name_class = version_name_class
 
     def read(self,
              name: str,
-             adapter: Union[Adapter[DataType], str],
-             version_name: Optional[Union[str, VersionName]] = None,
-             namespace: str = None) -> DataType:
-        pass
+             version_name: Optional[Union[str, VersionName]] = None) -> DataType:
+        versioned_artifact = VersionedArtifact(
+            artifact_name=name,
+            location=self.location,
+            datastore=self.datastore,
+        )
+        version = versioned_artifact.read(version_name=version_name)
+        return version
 
-    def write(data: DataType,
+    def write(self,
+              data: DataType,
               name: str,
-              adapter: Union[Adapter[DataType], str],
+              artifact_class: Type[Artifact],
               version_name: Optional[Union[str, VersionName]] = None,
-              namespace: str = None,
               inputs: Optional[Sequence[str]] = None,
               metadata: Optional[Dict[str, str]] = None,
-              save_mode: SaveMode = SaveMode.ERROR_IF_EXISTS) -> None:
-        pass
+              save_mode: SaveMode = SaveMode.ERROR_IF_EXISTS) -> Version:
+        versioned_artifact = VersionedArtifact(
+            artifact_name=name,
+            location=self.location,
+            datastore=self.datastore,
+            artifact_class=artifact_class,
+            version_name_class=self.version_name_class,
+        )
+        version = versioned_artifact.write(data, metadata, version_name=version_name)
+        return version
 
     # def export()
